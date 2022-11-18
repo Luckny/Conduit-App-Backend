3ec555402fc32d689e9c2a03b1e4a9a61c224186
Luckny <sluckny@gmail.com>
2022-11-18 11:59:25 -0500

    changed api default port from 3000 to 3200

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import App from "./App";
+import mongoose from "mongoose";
 
-const port = sanitizePort(process.env.PORT || 3000);
+const port = sanitizePort(process.env.PORT || 3200);
 
 console.log("Starting server...");
 console.log("Use https://api.realworld.io/api-docs/ to access api documentation");
 
 App.set("port", port);
 App.listen(port, () => {
-   console.log("Server started on port: ", port);
+   console.log(`Listening on http://localhost:${port}`);
+});
+
+// Database connection
+let localMongoURI: string = "mongodb://127.0.0.1:27017/conduit";
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const URI = process.env.DB_URI || localMongoURI;
+mongoose.connect(URI).then(() => {
+   console.log("Connected to database");
 });
 
 function sanitizePort(value: number | string): number | string | boolean {
