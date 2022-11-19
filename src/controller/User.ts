5f304dfc174fe5b5e5f55b183ab70be603595545
Luckny <sluckny@gmail.com>
2022-11-19 16:41:54 -0500

    added invalid parameter logic for register

--- src/controller/User.ts
+++ src/controller/User.ts
@@ -1,9 +1,14 @@
 import { User } from "../model/User";
 import { AlreadyExistsError } from "../../errors/alreadyExistsError";
+import { InvalidParameterError } from "../../errors/invalidParameterError";
 
 export class UserController {
    public async register(userInfo: registerInfo): Promise<any> {
       const { username, email, password } = userInfo;
+      if (!username || !email || !password)
+         throw new InvalidParameterError(
+            "a parameter for username, email or password might not have been provided"
+         );
       let user = await User.findOne({ username, email }); // checks to see if user already exist
       if (user) throw new AlreadyExistsError("user already exits");
       user = new User({ username, email, password });
