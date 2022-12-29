bd45c36382fd2232605223aa3a5b73a328d8c946
Luckny <sluckny@gmail.com>
2022-12-29 10:24:42 -0500

    asDTO function + minor updates

--- src/core/model/Article.ts
+++ src/core/model/Article.ts
@@ -1,6 +1,6 @@
 import { Schema, Types, model, Document } from "mongoose";
 import { iTag } from "./Tag";
-import { Profile } from "./User";
+import { iUser } from "./User";
 const uniqueValidator = require("mongoose-unique-validator");
 
 export interface iArticle extends Document {
@@ -9,21 +9,24 @@ export interface iArticle extends Document {
    description: string;
    body: string;
    tagList: Types.DocumentArray<iTag>;
-   favorited: boolean;
+   // favorited: boolean;
    favoritesCount: number;
-   author: Types.Subdocument<Profile>;
+   author: Types.Subdocument<iUser>;
    //    comments: Types.DocumentArray<Comment>;
+   asDTO(listOfTags: Types.DocumentArray<iTag>, user: iUser): Article;
 }
 
 export type Article = {
-   slug: string;
-   title: string;
-   description: string;
-   body: string;
-   tagList: Types.DocumentArray<iTag>;
-   favorited: boolean;
-   favoritesCount: number;
-   author: Types.Subdocument<Profile>;
+   article: {
+      slug: string;
+      title: string;
+      description: string;
+      body: string;
+      tagList: string[];
+      favorited: boolean;
+      favoritesCount: number;
+      author: any;
+   };
 };
 const articleSchema: Schema = new Schema<iArticle>(
    {
@@ -37,11 +40,26 @@ const articleSchema: Schema = new Schema<iArticle>(
       body: { type: String, required: true },
       tagList: [{ type: Types.ObjectId, ref: "Tag" }],
       favoritesCount: { type: Number, default: 0 },
-      author: { type: Types.ObjectId, ref: "Profile" },
+      author: { type: Types.ObjectId, ref: "User", required: true },
       //   comments: [{type: Types.ObjectId, ref: "Comment"}]
    },
    { timestamps: true }
 );
+
+articleSchema.methods.asDTO = function (listOfTags: Types.DocumentArray<iTag>, user: iUser): Article {
+   return {
+      article: {
+         slug: this.slug,
+         title: this.title,
+         description: this.description,
+         body: this.body,
+         tagList: listOfTags.map((tag) => tag.name),
+         favorited: user && user.favorites.includes(this._id),
+         favoritesCount: this.favoritesCount,
+         author: user.asProfileDTO(user.isFollowing(user._id)).profile,
+      },
+   };
+};
 
 articleSchema.plugin(uniqueValidator, { message: "already exists" });
 
