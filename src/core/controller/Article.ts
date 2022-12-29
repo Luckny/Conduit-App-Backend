76bb6829410ec6a0f0efa153e91aa8cca25edad1
Luckny <sluckny@gmail.com>
2022-12-29 10:23:11 -0500

    create article function

--- src/core/controller/Article.ts
+++ src/core/controller/Article.ts
@@ -1,18 +1,39 @@
 import { Article, iArticle } from "../model/Article";
 import slug = require("slug");
+import { Tag } from "../model/Tag";
+import { User } from "../model/User";
 // random string import
 export class ArticleController {
    public async createOne(articleInfo: ArticleCreateInfo, authorId: string): Promise<Article> {
       const { title, description, body, tagList } = articleInfo;
-      let article: iArticle = new Article({ slug: this.urlSlug(title), title, description, body, tagList });
-      article = await article.save();
+      const tagsId = (await this.transformTagListToTagId(tagList)) || [];
+      const author = await User.findById(authorId);
 
-      return article;
+      let article: iArticle = new Article({
+         slug: slug(title),
+         title,
+         description,
+         body,
+         tagList: tagsId,
+         author: authorId,
+      });
+
+      article = await (await article.save()).populate("tagList");
+      return article.asDTO(article.tagList, author);
    }
 
-   private urlSlug(title: string): string {
-      // let salt = cryptoRandomString({ length: 10, type: "url-safe" });
-      return slug(`${title}`);
+   private async transformTagListToTagId(tagList: string[]): Promise<string[]> {
+      if (!tagList || (tagList && tagList.length === 0)) return null;
+      return await Promise.all(
+         tagList.map(async (tagName: string) => {
+            let tag = await Tag.findOne({ name: tagName });
+            if (!tag) {
+               tag = new Tag({ name: tagName });
+               tag = await tag.save();
+            }
+            return tag._id;
+         })
+      );
    }
 }
 
